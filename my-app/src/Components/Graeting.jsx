80601222aab588fd3cb4c56dcc4914da826a19b1
Mennapika <mennana3im@gmail.com>
2022-06-06 22:17:17 +0200

    Created Greating COmponent

---
+++ my-app/src/Components/Graeting.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import thankYou from '../images/illustration-thank-you.svg';
+import "./Greating.css"
+
+export const Graeting = (props) => {
+  return (
+    <div className='Container'>
+        <div className='Greating-card'>
+        <img src={thankYou} />
+        <div className='Score--Container'>
+          <p className='Score--p'>
+            You selected {props.ratingValue} out of  5
+          </p>
+          </div>
+          <div className='ThankYou--Container'>
+              <h1>Thank You!</h1>
+              <p className='Text--P'>
+                  We appreciate you taking the time to give a rating. <br/>If you ever need more support.don't hesitate to <br/>get in touch!
+              </p>
+
+          </div>
+        </div>
+        
+    </div>
+  )
+}
