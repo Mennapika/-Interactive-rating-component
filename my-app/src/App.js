10b9d6c092ddf0fc226dec859d36f4ef2d13a2fd
Mennapika <mennana3im@gmail.com>
2022-06-06 22:17:40 +0200

    Created Logic

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,20 @@
 import Rating from './Components/Rating';
-import logo from './logo.svg';
+import React from 'react';
+import { Graeting } from './Components/Graeting';
+
 
 function App() {
+  const [sumbitted,SetSubmitted]=React.useState(false)
+  const [ratingValue,SetRatingValue]=React.useState(0)
+  function handleSumbited(rate){
+    SetSubmitted(true);
+    SetRatingValue(rate);
+  }
   return (
     <div className="App">
-       <Rating/>
+      {
+        sumbitted?<Graeting ratingValue={ratingValue}/>:<Rating handleSumbited={handleSumbited}/>
+      }
     </div>
   );
 }
--- my-app/src/Components/Rating.jsx
+++ my-app/src/Components/Rating.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import "./Rating.css"
 import star from '../images/icon-star.svg';
 
-const Rating = () => {
+const Rating = (props) => {
+const[value,SetValue]=React.useState(0);
   return (
     <div className='Container' >
     <div className='Card'>
@@ -10,17 +11,17 @@ const Rating = () => {
     <img src={star} />
     </div>
     <div className='Text--Container'>
-        <h1>How did we do ? </h1>
-        <p className='Feedback--P'>Please let us know how we did with your support reqeust.All feedback is approciated to help us improve our offering!</p>
+        <h1>How did we do? </h1>
+        <p className='Text--P'>Please let us know how we did with your support reqeust.All feedback is approciated to help us improve our offering!</p>
     </div>
     <div className='Button-container'>
-     <button className='Ratting--Button'>1</button>
-     <button  className='Ratting--Button'>2</button>
-     <button  className='Ratting--Button'>3</button>
-     <button  className='Ratting--Button'>4</button>
-     <button  className='Ratting--Button Sumbit--Button--5'>5</button>
+     <button className='Ratting--Button' onClick={()=>SetValue(1)}>1</button>
+     <button  className='Ratting--Button'onClick={()=>SetValue(2)}>2</button>
+     <button  className='Ratting--Button' onClick={()=>SetValue(3)}>3</button>
+     <button  className='Ratting--Button' onClick={()=>SetValue(4)}>4</button>
+     <button  className='Ratting--Button ' onClick={()=>SetValue(5)}>5</button>
     </div>
-    <button className='Sumbit--Button'>Submit</button>
+    <button className='Sumbit--Button'onClick={()=>props.handleSumbited(value)}>Submit</button>
     </div>
     </div>
   )
