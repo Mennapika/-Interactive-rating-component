044e7319396f82ff138cc86690088a3a6f9d71ba
Mennapika <mennana3im@gmail.com>
2022-06-06 23:11:25 +0200

    Created Mobile Style

--- my-app/src/Components/Graeting.jsx
+++ my-app/src/Components/Graeting.jsx
@@ -6,7 +6,7 @@ export const Graeting = (props) => {
   return (
     <div className='Container'>
         <div className='Greating-card'>
-        <img src={thankYou} />
+        <img className='Greating-img' src={thankYou} />
         <div className='Score--Container'>
           <p className='Score--p'>
             You selected {props.ratingValue} out of  5
@@ -15,7 +15,7 @@ export const Graeting = (props) => {
           <div className='ThankYou--Container'>
               <h1>Thank You!</h1>
               <p className='Text--P'>
-                  We appreciate you taking the time to give a rating. <br/>If you ever need more support.don't hesitate to <br/>get in touch!
+                  We appreciate you taking the time to give a rating.If you ever need more support.don't hesitate to get in touch!
               </p>
 
           </div>
